0ef189d29e00a585fad154893ae71f294a13359f
Kelian DUMARAIS (MLforAll) <kdumarai@student.42.fr>
2020-01-20 18:47:14 +0100

    Added tests for ft_list_remove_if -> this bonus function doesn't work

---
+++ tests/ft_list_remove_if/main.c
@@ -0,0 +1,125 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdumarai <kdumarai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/09 15:47:47 by kelian            #+#    #+#             */
+/*   Updated: 2020/01/20 18:34:20 by kdumarai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "libasm.h"
+#include "libasm_bonus.h"
+
+static void		ft_print_result(t_list *lst)
+{
+	if (!lst)
+	{
+		(void)puts("ft_print_result: empty");
+		return ;
+	}
+	while (lst)
+	{
+		(void)puts((const char *)lst->data);
+		lst = lst->next;
+	}
+}
+
+static t_list	*ft_list_newnode(void *data)
+{
+	t_list	*ret;
+
+	if (!(ret = (t_list *)malloc(sizeof(t_list))))
+		return NULL;
+	ret->data = data;
+	ret->next = NULL;
+	return ret;
+}
+
+static void		ft_list_push_back(t_list **lst, t_list *node)
+{
+	t_list	*bw;
+
+	if (!lst)
+		return ;
+	if (!*lst)
+	{
+		*lst = node;
+		return ;
+	}
+	bw = *lst;
+	while (bw->next)
+		bw = bw->next;
+	bw->next = node;
+}
+
+static void		ft_list_release(t_list **lst)
+{
+	t_list	*l;
+	t_list	*tmp;
+
+	if (!lst)
+		return ;
+	l = *lst;
+	while (l)
+	{
+		tmp = l->next;
+		free(l);
+		l = tmp;
+	}
+	*lst = NULL;
+}
+
+static int		remif_cb(void *a, void *b)
+{
+	return strcmp((const char *)a, (const char *)b);
+}
+
+int				main(int argc, const char *argv[])
+{
+	t_list	*list;
+
+	if (argc == 1)
+		return EXIT_FAILURE;
+	alarm(5);
+	list = ft_list_newnode("yolo");
+	ft_list_push_back(&list, ft_list_newnode("welcome42"));
+	ft_list_push_back(&list, ft_list_newnode("foobar"));
+	ft_list_push_back(&list, ft_list_newnode("salutLesGars"));
+	ft_list_push_back(&list, ft_list_newnode("AssemblyYourself"));
+	ft_list_push_back(&list, ft_list_newnode("libasm or libftasm?!"));
+	switch (atoi(argv[1]))
+	{
+		case 1:
+			ft_list_remove_if(&list, "welcome42", &remif_cb);
+			break ;
+		case 2:
+			ft_list_remove_if(&list, "yolo", &remif_cb);
+			break ;
+		case 3:
+			ft_list_remove_if(&list, "libasm or libftasm?!", &remif_cb);
+			break ;
+		case 4:
+			ft_list_remove_if(&list, "notfound", &remif_cb);
+			break ;
+		case 5:
+			ft_list_remove_if(&list, "foobar", &remif_cb);
+			break ;
+		case 6:
+			ft_list_release(&list);
+			list = ft_list_newnode("say something");
+			ft_list_remove_if(&list, "say something", &remif_cb);
+			break ;
+		default:
+			break ;
+	}
+	ft_print_result(list);
+	return EXIT_SUCCESS;
+}
--- tests/grademe.sh
+++ tests/grademe.sh
@@ -1,38 +1,98 @@
 #!/bin/bash
 
-echo '=> Make lib'
+msg()
+{
+	local clr
+	case "$1" in
+		w )
+			clr='\033[1;33m'
+			;;
+		e )
+			clr='\033[1;31m'
+			;;
+		g )
+			clr='\033[1;32m'
+			;;
+		i )
+			clr='\033[1;39m'
+			;;
+	esac
+
+	local nlc='\n'
+	if [ "$2" = '-n' -a ! -z "$3" ]
+	then
+		nlc=''
+		shift
+	fi
+
+	if [ ! -t 1 ] || ! echo "$TERM" | grep xterm >/dev/null
+	then
+		echo "$2"
+		return
+	fi
+	printf "${clr}%s\033[0;39m$nlc" "$2"
+}
+
+
+msg i '=> Make lib'
 make -C .. bonus
 echo
+
+msg i '=> Run initial_tests'
+gcc -o initial_tests initial_tests.c -L.. -lasm -I../includes
+[ $? -eq 0 ] || exit 0
+./initial_tests
+rm -f initial_tests
 
 for t in *
 do
 	[ -d "$t" ] || continue
-	echo "===> TEST $t"
+
+	echo
+	msg i "===> TEST $t"
 	cd "$t"
+
 	gcc -o test main.c -L../.. -lasm -I../../includes
 	if [ $? -eq 0 ]
 	then
 		ls -1 test*.output | sort | while read testfile
 		do
 			testdiff="${testfile%output}"mine
-			./test $(echo "$testfile" | sed 's/[a-zA-Z.]*//g') > "$testdiff"
-			diff "$testdiff" "$testfile" >/dev/null 2>&1
-			if [ $? -eq 0 ]
+			./test $(echo "$testfile" | sed 's/[a-zA-Z.]*//g') > "$testdiff" |:
+			status=${PIPESTATUS[0]}
+			if [ $status -gt 128 ]
 			then
-				echo -n .
-			else
-				echo -n X
+				case $status in
+					134 )
+						msg e -n A
+						;;
+					139 )
+						msg e -n S
+						;;
+					138 )
+						msg e -n B
+						;;
+					142 )
+						msg e -n T
+						;;
+					* )
+						msg e -n C
+						;;
+				esac
+				continue
 			fi
+			diff "$testdiff" "$testfile" >/dev/null 2>&1
+			case $? in
+				0 )
+					msg g -n .
+					;;
+				1 )
+					msg e -n X
+					;;
+			esac
 		done
 		echo
 		rm -f test
 	fi
 	cd - >/dev/null
-	echo
 done
-
-echo '=> Run initial_tests'
-gcc -o initial_tests initial_tests.c -L.. -lasm -I../includes
-[ $? -eq 0 ] || exit 0
-./initial_tests
-rm -f initial_tests
